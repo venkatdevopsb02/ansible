39377c7980b4060039f5a8d589ee0715f182e74c
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-02-15 11:38:30 +0000

    Feb 15, 2023, 6:38 AM

--- install.sh
+++ install.sh
@@ -11,3 +11,6 @@ ansible -i hosts jumpbox -m ping -u centos -k
 ### How to check - whether a package is installed or not. 
 ansible -i hosts jumpbox -u centos -k -m ansible.builtin.yum -a "name=httpd"
 ansible -i hosts jumpbox -u centos -kK -m ansible.builtin.yum -a "name=httpd"
+
+
+git pull; sed -i -e "s/dummy/DevOps654321/g" hosts ; ansible-playbook  -i hosts -b 02-vars.yml ; sed -i -e "s/DevOps654321/dummy/g" hosts
